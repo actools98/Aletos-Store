8c8d6ae9c38dd5d79ba57915149fe25138c051c0
actools98 <actools1998@gmail.com>
2026-09-22 21:29:49 -0500

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,10 @@
 export const eur = (n: number) =>
-  new Intl.NumberFormat('es-ES', { style: 'currency', currency: 'EUR' }).format(n || 0)
+  new Intl.NumberFormat('es-CO', {
+    style: 'currency',
+    currency: 'COP',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(n || 0)
 
 export const slugify = (s: string) =>
   s.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '')
